14dcb499c3ad414f8a06809a19336b48443043a8
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-05 01:27:31 +0545

    create routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
-function App() {
-  return <></>;
-}
+import { createBrowserRouter, RouterProvider } from "react-router";
+import HomePage from "./pages/home-page";
+import LoginPageAdmin from "./pages/login/login-admin";
+import LoginPageUser from "./pages/login/login-user";
+import LoginPage from "./pages/login";
 
-export default App;
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+    children: [
+      {
+        path: "admin",
+        element: <LoginPageAdmin />,
+      },
+      {
+        path: "user",
+        element: <LoginPageUser />,
+      },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
