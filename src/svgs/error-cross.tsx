957c52ceb1cf2d71beb1255d13118df27b8b2bf2
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-06 02:22:00 +0545

    create error cross svg component

---
+++ src/svgs/error-cross.tsx
@@ -0,0 +1,24 @@
+export function ErrorCross() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.25"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="align-middle inline"
+    >
+      <circle
+        cx="12"
+        cy="12"
+        r="10"
+      />
+      <path d="m15 9-6 6" />
+      <path d="m9 9 6 6" />
+    </svg>
+  );
+}
