285d44513c1ae6c476aa55c5609db300ad192ee5
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-06 02:18:05 +0545

    create required routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,69 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
-import HomePage from "./pages/home-page";
-import LoginPageAdmin from "./pages/login/login-admin";
-import LoginPageUser from "./pages/login/login-user";
-import LoginPage from "./pages/login";
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router';
+import HomePage from './pages/home-page';
+import LoginPageAdmin from './pages/login/login-admin';
+import LoginPageUser from './pages/login/login-user';
+import LoginPage from './pages/login';
+import {
+  AdminProtectedRoutes,
+  NonAuthenticatedRoutes,
+  PublicProtectedRoutes,
+} from './components/protected-routes';
+import NotFound from './pages/not-found';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <HomePage />,
   },
   {
-    path: "/login",
+    path: '/home',
+    element: <Navigate to="/" />,
+  },
+  {
+    path: '/login',
     element: <LoginPage />,
     children: [
       {
-        path: "admin",
-        element: <LoginPageAdmin />,
+        index: true,
+        element: <NotFound />,
       },
       {
-        path: "user",
-        element: <LoginPageUser />,
+        path: 'user',
+        element: (
+          <NonAuthenticatedRoutes>
+            <LoginPageUser />
+          </NonAuthenticatedRoutes>
+        ),
+      },
+      {
+        path: 'admin',
+        element: (
+          <NonAuthenticatedRoutes>
+            <LoginPageAdmin />
+          </NonAuthenticatedRoutes>
+        ),
       },
     ],
+  },
+  {
+    path: '/admin',
+    element: (
+      <AdminProtectedRoutes>
+        <></>
+      </AdminProtectedRoutes>
+    ),
+  },
+  {
+    path: '/user',
+    element: (
+      <PublicProtectedRoutes>
+        <></>
+      </PublicProtectedRoutes>
+    ),
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ]);
 
