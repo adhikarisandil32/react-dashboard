b07c28aeb6a9e48f265963e4b1606a37bcad5132
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-07 00:32:11 +0545

    add event listener to document when profile menu is rendered

--- src/components/commons/profile-menu.tsx
+++ src/components/commons/profile-menu.tsx
@@ -1,16 +1,17 @@
-import { useAuthStore, type AuthType } from '@root/src/stores/auth-store';
-import { getDashboardPath } from '@root/src/utils/functions';
-import { clearLocalStorage } from '@root/src/utils/local-storage';
+import { useAuthStore } from '@src/stores/auth-store';
 import { ClipboardList, LogOut, User } from 'lucide-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router';
+import { clearLocalStorage } from '@src/utils/local-storage';
+import { getDashboardPath } from '@src/utils/functions';
+// import Modal from './modal';
 
 export default function ProfileMenu() {
   const { auth } = useAuthStore();
   const navigate = useNavigate();
 
   return auth ? (
-    <AuthenticatedUser auth={auth} />
+    <AuthenticatedUser />
   ) : (
     <button
       className="border border-indigo-600 text-gray-700 px-6 py-2 rounded-lg hover:bg-indigo-50 transition-colors cursor-pointer"
@@ -21,45 +22,67 @@ export default function ProfileMenu() {
   );
 }
 
-function AuthenticatedUser({ auth }: { auth: AuthType }) {
+function AuthenticatedUser() {
   const [showMenu, setShowMenu] = useState<boolean>(false);
 
-  const handleLogout = () => {
-    clearLocalStorage();
-    hideProfileMenu();
+  const toggleMenu = (e: React.MouseEvent) => {
+    e.stopPropagation(); // Prevent click from bubbling to document
+    setShowMenu((prev) => !prev);
   };
-
-  const hideProfileMenu = () => setShowMenu(false);
 
   return (
     <div className="relative inline-block">
       <button
-        onClick={() => setShowMenu((prev) => !prev)}
-        className="cursor-pointer align-middle"
+        onClick={toggleMenu}
+        className="p-1 cursor-pointer align-middle rounded-full border border-black"
       >
         <User />
       </button>
 
-      {showMenu && (
-        <div className="absolute right-0 top-[calc(100%+10px)] border border-gray-400 bg-gray-50 rounded-md p-1 min-w-40">
-          <Link
-            to={getDashboardPath(auth.role) ?? '/'}
-            className="inline-block px-3 py-2 hover:bg-gray-100 rounded-sm w-full text-left space-x-1"
-            onClick={hideProfileMenu}
-          >
-            <ClipboardList className="inline align-middle size-5" />
-            <span>Dashboard</span>
-          </Link>
+      {showMenu && <Menu setShowMenu={setShowMenu} />}
+    </div>
+  );
+}
 
-          <button
-            className="cursor-pointer inline-block px-3 py-2 hover:bg-red-200 rounded-sm w-full text-left space-x-1 text-red-600 bg-red-100"
-            onClick={handleLogout}
-          >
-            <LogOut className="inline align-middle size-5" />
-            <span>Logout</span>
-          </button>
-        </div>
-      )}
+function Menu({
+  setShowMenu,
+}: {
+  setShowMenu: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const { auth, setAuth } = useAuthStore();
+
+  const handleLogout = () => {
+    clearLocalStorage();
+    setAuth(undefined);
+  };
+
+  const hideProfileMenu = () => setShowMenu(false);
+
+  useEffect(() => {
+    document.addEventListener('click', hideProfileMenu);
+
+    return () => {
+      document.removeEventListener('click', hideProfileMenu);
+    };
+  }, []);
+
+  return (
+    <div className="absolute right-0 top-[calc(100%+10px)] border border-gray-400 bg-gray-50 rounded-md p-1 min-w-40">
+      <Link
+        to={getDashboardPath(auth!.role) ?? '/'}
+        className="inline-block px-3 py-2 hover:bg-gray-100 rounded-sm w-full text-left space-x-1"
+      >
+        <ClipboardList className="inline align-middle size-5" />
+        <span>Dashboard</span>
+      </Link>
+
+      <button
+        className="cursor-pointer inline-block px-3 py-2 hover:bg-red-200 rounded-sm w-full text-left space-x-1 text-red-600 bg-red-100"
+        onClick={handleLogout}
+      >
+        <LogOut className="inline align-middle size-5" />
+        <span>Logout</span>
+      </button>
     </div>
   );
 }
