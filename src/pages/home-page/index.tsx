bbdc90312c59811e73413b9f1a6cb83bbc36ebaf
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-07 00:32:26 +0545

    fix imports

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -1,8 +1,8 @@
-import BlogsSection from '@root/src/components/home-page/blog-section';
-import Footer from '@root/src/components/commons/footer';
-import Header from '@root/src/components/commons/header';
-import HeroSection from '@root/src/components/home-page/hero-section';
-import OurWorksSection from '@root/src/components/home-page/our-works-section';
+import Footer from '@src/components/commons/footer';
+import Header from '@src/components/commons/header';
+import BlogsSection from '@src/components/page-wise/home-page/blog-section';
+import HeroSection from '@src/components/page-wise/home-page/hero-section';
+import OurWorksSection from '@src/components/page-wise/home-page/our-works-section';
 
 export default function HomePage() {
   return (
