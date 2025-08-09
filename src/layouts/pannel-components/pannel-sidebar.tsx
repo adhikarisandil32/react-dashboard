312d64d7880c0f3a7524166e30f90ab5de55aad4
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-10 02:44:50 +0545

    add logic to use different width if menu is scrollable

--- src/layouts/pannel-components/pannel-sidebar.tsx
+++ src/layouts/pannel-components/pannel-sidebar.tsx
@@ -26,37 +26,21 @@ function PannelSidebarDesktop({ children }: { children: React.ReactNode }) {
   }, []);
 
   return (
-    <div>
-      <div
-        className="h-screen bg-gray-900 space-y-4 p-4 text-gray-200 transition-[width] duration-300"
-        style={{
-          width: isSidebarExpanded
-            ? expandedSidebarWidth
-            : isSidebarMenuOverflowing
-            ? collapsedScrollableSidebarWidth
-            : collapsedSidebarWidth,
-        }}
-        data-expanded={isSidebarExpanded}
-      >
-        <div className="min-h-12 flex items-center justify-between cursor-pointer rounded-md p-1 px-2 hover:bg-gray-800">
-          <div className="flex items-center gap-2">
-            <div className="size-10 rounded-full border border-gray-200 grid place-items-center">
-              AU
-            </div>
-
-            <AnimatePresence>
-              {isSidebarExpanded && (
-                <motion.div
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                  transition={{ duration: 0.3 }}
-                >
-                  <p className="font-bold">Admin User</p>
-                  <p className="text-sm">dummy@email.com</p>
-                </motion.div>
-              )}
-            </AnimatePresence>
+    <div
+      className="h-screen bg-gray-900 space-y-4 p-4 text-gray-200 transition-[width] duration-300"
+      style={{
+        width: isSidebarExpanded
+          ? expandedSidebarWidth
+          : isSidebarMenuOverflowing
+          ? collapsedScrollableSidebarWidth
+          : collapsedSidebarWidth,
+      }}
+      data-expanded={isSidebarExpanded}
+    >
+      <div className="min-h-12 flex items-center justify-between cursor-pointer rounded-md p-1 px-2 hover:bg-gray-800">
+        <div className="flex items-center gap-2">
+          <div className="size-10 rounded-full border border-gray-200 grid place-items-center">
+            AU
           </div>
 
           <AnimatePresence>
@@ -67,36 +51,50 @@ function PannelSidebarDesktop({ children }: { children: React.ReactNode }) {
                 exit={{ opacity: 0 }}
                 transition={{ duration: 0.3 }}
               >
-                <ChevronsUpDown className="size-5" />
+                <p className="font-bold">Admin User</p>
+                <p className="text-sm">dummy@email.com</p>
               </motion.div>
             )}
           </AnimatePresence>
         </div>
 
-        <div className="space-y-1.5">
-          <div className="h-5">
-            <AnimatePresence>
-              {isSidebarExpanded && (
-                <motion.div
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                  transition={{ duration: 0.3 }}
-                >
-                  <span className="text-sm font-semibold whitespace-nowrap">
-                    My Pannel
-                  </span>
-                </motion.div>
-              )}
-            </AnimatePresence>
-          </div>
+        <AnimatePresence>
+          {isSidebarExpanded && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.3 }}
+            >
+              <ChevronsUpDown className="size-5" />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
 
-          <div
-            className="h-[calc(100vh-124px)] overflow-y-auto"
-            ref={menuRef}
-          >
-            {children}
-          </div>
+      <div className="space-y-1.5">
+        <div className="h-5">
+          <AnimatePresence>
+            {isSidebarExpanded && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.3 }}
+              >
+                <span className="text-sm font-semibold whitespace-nowrap">
+                  My Pannel
+                </span>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
+
+        <div
+          className="h-[calc(100vh-124px)] overflow-y-auto"
+          ref={menuRef}
+        >
+          {children}
         </div>
       </div>
     </div>
