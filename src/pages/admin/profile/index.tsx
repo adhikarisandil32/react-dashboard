1634a510aacb56ab69954429282a313d76da17aa
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-10 18:20:19 +0545

    profile info page for admin and public user page

---
+++ src/pages/admin/profile/index.tsx
@@ -0,0 +1,27 @@
+import { useAuthStore } from '@src/stores/auth-store';
+
+export default function AdminProfilePage() {
+  const { auth } = useAuthStore();
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-6">Admin Profile</h2>
+
+      <div className="border border-gray-200 p-6 rounded-md space-y-2">
+        <div className="flex">
+          <p className="font-semibold w-20">Name: </p>
+          <p className="text-gray-600">{auth?.name}</p>
+        </div>
+
+        <div className="flex">
+          <p className="font-semibold w-20">Email: </p>
+          <p className="text-gray-600">{auth?.email}</p>
+        </div>
+
+        <div className="flex">
+          <p className="font-semibold w-20">Role: </p>
+          <p className="text-gray-600">{auth?.role}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
