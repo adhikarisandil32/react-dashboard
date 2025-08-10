e9609255ac5417e21a5e62db58c406e10cc50de1
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-10 16:24:34 +0545

    privde absolute path with '/' instead of relative

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,10 +1,23 @@
 export const getDashboardPath = (userRole: 'admin' | 'user') => {
   switch (userRole) {
     case 'user':
-      return 'user/dashboard';
+      return '/user/dashboard';
 
     case 'admin':
-      return 'admin/dashboard';
+      return '/admin/dashboard';
+
+    default:
+      return undefined;
+  }
+};
+
+export const getProfilePath = (userRole: 'admin' | 'user') => {
+  switch (userRole) {
+    case 'user':
+      return '/user/profile';
+
+    case 'admin':
+      return '/admin/profile';
 
     default:
       return undefined;
